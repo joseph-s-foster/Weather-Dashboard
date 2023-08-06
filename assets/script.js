4e6dbb238f2f73a7da830ead949cf480b032a373
D3C0D1NG <joseph.s.foster@icloud.com>
2023-08-06 10:44:03 -0500

    refactored js

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,70 @@
 const apikey = "7761e5644bf2af39970d6760ed459312";
-function handleSearch() {
-    let city = document.querySelector(".search input").value
-    if (!city) return;
-    fetchCurrentWeather(city);
+
+const handleSearch = async () => {
+  const city = document.querySelector(".search input").value;
+  if (!city) return;
+  const currentWeatherData = await fetchWeatherData(
+    `https://api.openweathermap.org/data/2.5/weather?appid=${apikey}&q=${city}&units=imperial`
+  );
+  if (currentWeatherData) {
+    displayCurrentWeather(currentWeatherData);
+    const forecastData = await fetchWeatherData(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${currentWeatherData.coord.lat}&lon=${currentWeatherData.coord.lon}&units=imperial&appid=${apikey}`
+    );
+    if (forecastData) displayForecast(forecastData);
+  }
 };
 
-function fetchCurrentWeather(city) {
-    const apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apikey}&q=${city}&units=imperial`
-    fetch(apiUrlWeather).then(function (res) {
-        return res.json()
-    }).then(function (data) {
-        console.log(data)
-        displayCurrentWeather(data)
-        let lat = data.coord.lat
-        let lon = data.coord.lon
-        const apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apikey}`
-        fetch(apiUrlForecast).then(function (res) {
-            return res.json()
-        }).then(function (data) {
-            console.log(data)
-        })
-    })
+const fetchWeatherData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) return await response.json();
+    throw new Error("Network response was not ok.");
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return null;
+  }
 };
 
-function displayCurrentWeather(data) {
-    const h2El = document.createElement("h2")
-    h2El.textContent = `${data.name} ${data.dt} `
-    let iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-    const icon = document.createElement("img")
-    icon.src = iconUrl
-    const tempEl = document.createElement("p")
-    tempEl.textContent = `temperature: ${data.main.temp} F`
-    const humidityEl = document.createElement("p")
-    humidityEl.textContent = `humdity: ${data.main.humidity} %`
-    const windSpeedEl = document.createElement("p")
-    windSpeedEl.textContent = `wind speed: ${data.wind.speed} mph`
-    document.querySelector(".current").append(h2El,icon,tempEl,humidityEl,windSpeedEl)
+const convertUnixTimestampToDate = (timestamp) => {
+  const date = new Date(timestamp * 1000);
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
 };
 
-document.querySelector(".search button").addEventListener("click", handleSearch)
+const displayCurrentWeather = (data) => {
+  const { name, dt, weather, main, wind } = data;
+  const iconUrl = `https://openweathermap.org/img/w/${weather[0].icon}.png`;
+  const formattedDate = convertUnixTimestampToDate(dt);
+  const currentEl = document.querySelector(".current");
+  currentEl.innerHTML = `
+    <h2>${name} ${formattedDate}</h2>
+    <img src="${iconUrl}" alt="Weather Icon">
+    <p>temperature: ${main.temp} F</p>
+    <p>humidity: ${main.humidity} %</p>
+    <p>wind speed: ${wind.speed} mph</p>
+  `;
+};
+
+const displayForecast = (data) => {
+  const forecastEl = document.querySelector(".forecast");
+  forecastEl.innerHTML = "";
+  for (let i = 0; i < 5; i++) {
+    const { city, list } = data;
+    const { name } = city;
+    const { dt, weather, main, wind } = list[i * 8];
+    const iconUrl = `https://openweathermap.org/img/w/${weather[0].icon}.png`;
+    const formattedDate = convertUnixTimestampToDate(dt);
+    forecastEl.innerHTML += `
+      <h3>${name} ${formattedDate}</h3>
+      <img src="${iconUrl}" alt="Weather Icon">
+      <p>temperature: ${main.temp} F</p>
+      <p>humidity: ${main.humidity} %</p>
+      <p>wind speed: ${wind.speed} mph</p>
+    `;
+  }
+};
+
+document.querySelector(".search button").addEventListener("click", handleSearch);
